726dddb4edf5071d8b88ba1f4467b9d591a5258e
Calvin Tran <calvintran1478@protonmail.com>
2024-03-23 16:53:55 -0400

    Fix server issue due to dropped types

--- server/models/setup.go
+++ server/models/setup.go
@@ -24,23 +24,15 @@ func SetupModels() *gorm.DB {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", dbHost, dbUser, dbPassword, dbName, dbPort)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect to database")
+		panic("Failed to connect to database")
 	}
 
 	// Create enum types
-	db.Exec("DROP TYPE IF EXISTS gender CASCADE;")
-	db.Exec("DROP TYPE IF EXISTS hair_colour CASCADE;")
-	db.Exec("DROP TYPE IF EXISTS skin_colour CASCADE;")
-	db.Exec("DROP TYPE IF EXISTS eye_colour CASCADE;")
-	db.Exec("DROP TYPE IF EXISTS weapon_type CASCADE;")
-	db.Exec("DROP TYPE IF EXISTS armor_type CASCADE;")
-
-	db.Exec("CREATE TYPE gender AS ENUM('male', 'female');")
-	db.Exec("CREATE TYPE hair_colour AS ENUM('black', 'brown', 'blonde', 'white', 'gray');")
-	db.Exec("CREATE TYPE skin_colour AS ENUM('pale', 'tan', 'dark');")
-	db.Exec("CREATE TYPE eye_colour AS ENUM('black', 'blue', 'green', 'silver');")
-	db.Exec("CREATE TYPE weapon_type AS ENUM('sword', 'axe', 'spear', 'dagger', 'staff', 'bow');")
-	db.Exec("CREATE TYPE armor_type AS ENUM('helmet', 'body_armor', 'leg_armor', 'boots');")
+	setupFile, err := os.ReadFile("./database/setup.sql")
+	if err != nil {
+		log.Fatalf("Error loading SQL setup file")
+	}
+	db.Exec(string(setupFile))
 
 	// Migrate schemas
 	db.AutoMigrate(&User{})
